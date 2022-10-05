00640777440f3c1fffde7b29d38b01b0df8fc68b
CosmicJay2k <89969779+CosmicJay2k@users.noreply.github.com>
2022-10-05 15:09:49 +0200

    Add test

--- app/src/test/java/labb6/AppTest.java
+++ app/src/test/java/labb6/AppTest.java
@@ -3,10 +3,38 @@ package labb6;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.net.InetAddress;
+import java.net.NetworkInterface;
+
+import org.junit.jupiter.api.BeforeAll;
+
 class AppTest {
+    static InetAddress group;
+    static int port;
+    static NetworkInterface netIf;
+
+    @BeforeAll
+    public static void setup() {
+        try {
+            group = InetAddress.getByName("239.0.0.0");
+            port = Integer.parseInt("1234");
+            netIf = NetworkInterface.getByName("bge0");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
     @Test
-    void appHasAGreeting() {
-        App classUnderTest = new App();
-        // assertNotNull(classUnderTest.getGreeting(), "app should have a greeting");
+    void appCanStartMulticastSocketWhenGivenValidParameters() {
+        try {
+            group = InetAddress.getByName("239.0.0.0");
+            port = Integer.parseInt("1234");
+            netIf = NetworkInterface.getByName("bge0");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
+        assertDoesNotThrow(() -> App.startMulticast(group, port, netIf));
+
     }
 }
